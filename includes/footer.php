f41f334197e9fcfa7ba790904b7bf1ab85136dcc
Tatiani Cunha <tatianicpsantana@gmail.com>
2025-09-23 01:59:22 -0300

    Alterações na responsividade e no css do tipo de receita

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> Receitas de Família. Todos os direitos reservados.</p>
-  </footer>
-</body>
-</html>
--- partials/_footer.php
+++
@@ -1,9 +0,0 @@
-<?php /* partials/_footer.php */ ?>
-
-<footer class="rodape">
-    <p>@2025 - StoryBites - Receitas que Contam Histórias</p>   
-    <p>Todos os direitos reservados. Segurança e privacidade.</p>
-</footer>
-
-</body>
-</html>
